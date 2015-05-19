76fd5317f50c9c0c8325f49d52d15df9a7ce49cf
shaarmann <stephan.haarmann@student.hpi.uni-potsdam.de>
2015-05-19 09:33:22 +0200

    Added comments to ControlFlow and Refactored. Intern the setters and getters are used as well to check the conditions

--- src/main/java/de/uni_potsdam/hpi/bpt/bp2014/conversion/activity_centric/ControlFlow.java
+++ src/main/java/de/uni_potsdam/hpi/bpt/bp2014/conversion/activity_centric/ControlFlow.java
@@ -3,13 +3,28 @@ package de.uni_potsdam.hpi.bpt.bp2014.conversion.activity_centric;
 import de.uni_potsdam.hpi.bpt.bp2014.conversion.IEdge;
 import de.uni_potsdam.hpi.bpt.bp2014.conversion.INode;
 
+/**
+ * This class represents a control flow edge.
+ * It connects to nodes which can be connected by control flow.
+ * Supported types are {@link Activity}, {@link Gateway}, {@link Event}.
+ */
 public class ControlFlow implements IEdge {
     public INode source;
     public INode target;
 
+    /**
+     * Creates a new instance of the control flow
+     * with specified source and target.
+     * Be aware that both nodes must not be null and
+     * should meet the constraints, such that they are
+     * not null and instances of one of the supported types.
+     *
+     * @param source The source of the new Edge.
+     * @param target The target of the new Edge.
+     */
     public ControlFlow(INode source, INode target) {
-        this.source = source;
-        this.target = target;
+        setSource(source);
+        setTarget(target);
     }
 
     @Override
@@ -17,6 +32,13 @@ public class ControlFlow implements IEdge {
         return source;
     }
 
+    /**
+     * Set the source of the edge.
+     *
+     * @param source The new source.
+     *               Pre: The node must not be null.
+     *               the node must be of a supported type.
+     */
     @Override
     public void setSource(INode source) {
         assert null != source :
@@ -33,6 +55,14 @@ public class ControlFlow implements IEdge {
         return target;
     }
 
+
+    /**
+     * Set the target of the edge.
+     *
+     * @param target The new source.
+     *               Pre: The node must not be null.
+     *               the node must be of a supported type.
+     */
     @Override
     public void setTarget(INode target) {
         assert null != target :

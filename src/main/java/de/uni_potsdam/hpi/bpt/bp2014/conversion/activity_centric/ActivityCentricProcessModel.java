570c677e7887898735760b84ffe4709c7e023eed
shaarmann <stephan.haarmann@student.hpi.uni-potsdam.de>
2015-05-19 09:25:54 +0200

    Added java doc to ActivityCentricProcessModel.java

--- src/main/java/de/uni_potsdam/hpi/bpt/bp2014/conversion/activity_centric/ActivityCentricProcessModel.java
+++ src/main/java/de/uni_potsdam/hpi/bpt/bp2014/conversion/activity_centric/ActivityCentricProcessModel.java
@@ -6,10 +6,35 @@ import de.uni_potsdam.hpi.bpt.bp2014.conversion.INode;
 
 import java.util.*;
 
-
+/**
+ * This class represents an Activity Centric Process Models such as
+ * a BPMN model.
+ * It consists of different nodes which can be connected using various
+ * Edges.
+ * Supported nodes are {@link Activity}, {@link Event}, {@link Gateway} and
+ * {@link DataObject}.
+ */
 public class ActivityCentricProcessModel implements IModel {
+    /**
+     * A Variable to hold the start event of the model.
+     * The start event will also be the start node of the model.
+     * Hence the final model must contain a start event.
+     */
     private Event startEvent;
+
+    /**
+     * A Variable to hold the end event of the model.
+     * The start event will also be the final node of the model.
+     * Hence the final model must contain a end event.
+     * Also multiple end events are not supported.
+     */
     private Event endEvent;
+
+    /**
+     * A Variable to hold the nodes of the Process model.
+     * Be aware that the start and end event must be added
+     * to this collection as well.
+     */
     private Set<INode> nodes;
 
     public ActivityCentricProcessModel() {
@@ -21,6 +46,15 @@ public class ActivityCentricProcessModel implements IModel {
         return new ArrayList<>(nodes);
     }
 
+    /**
+     * Adds a new Node to the processModel.
+     * Duplicated nodes will be detected an removed.
+     *
+     * @param newNode The node to be added.
+     *                Pre: new node must not be null
+     *                     new node must be either of type Activity, Gateway,
+     *                     Event or DataObject (Hierarchies are supported)
+     */
     @Override
     public void addNode(INode newNode) {
         assert null != newNode :
@@ -34,6 +68,14 @@ public class ActivityCentricProcessModel implements IModel {
         this.nodes.add(newNode);
     }
 
+    /**
+     * Returns a new list of nodes which fit to the specified
+     * @param clazz
+     * @param <T> The type of the returned list (should be compatible to clazz)
+     * @return The newly created list of nodes.
+     * Changing the list will not affect the state of the model.
+     * Nevertheless altering the elements may lead to a corrupt state.
+     */
     @Override
     public <T extends INode> List<T> getNodesOfClass(Class clazz) {
         List<T> resultNodes = new ArrayList<>();
@@ -50,6 +92,13 @@ public class ActivityCentricProcessModel implements IModel {
         return startEvent;
     }
 
+    /**
+     * Set the start Event of the Activity Model.
+     * @param startNode The node which represents the start node.
+     *                  Pre: the node must not be null;
+     *                       the node must be an Event;
+     *                       the event must have the type Start;
+     */
     @Override
     public void setStartNode(INode startNode) {
         assert null != startNode :
@@ -62,6 +111,14 @@ public class ActivityCentricProcessModel implements IModel {
         this.startEvent = (Event)startNode;
     }
 
+
+    /**
+     * Set the end Event of the Activity Model.
+     * @param finalNode The node which represents the end event.
+     *                  Pre: the node must not be null;
+     *                       the node must be an Event;
+     *                       the event must have the type End;
+     */
     @Override
     public void addFinalNode(INode finalNode) {
         assert null != finalNode :
@@ -74,6 +131,10 @@ public class ActivityCentricProcessModel implements IModel {
         this.endEvent = (Event)finalNode;
     }
 
+    /**
+     * Returns a new list containing the end Event.
+     * @return A List with the end event or null.
+     */
     @Override
     public List<INode> getFinalStates() {
         List<INode> finalNodes = new ArrayList<>(1);
@@ -81,6 +142,11 @@ public class ActivityCentricProcessModel implements IModel {
         return finalNodes;
     }
 
+
+    /**
+     * Returns a new list containing the end Event.
+     * @return A List with the end event or an empty list.
+     */
     @Override
     public <T extends INode> List<T> getFinalNodesOfClass(Class t) {
         if (t.isAssignableFrom(Event.class)) {
@@ -90,6 +156,11 @@ public class ActivityCentricProcessModel implements IModel {
         }
     }
 
+    /**
+     * Returns a list with all edges of the model.
+     * Therefor, we iterate over the outgoing edges of all nodes.
+     * @return A new List with all edges.
+     */
     public List<IEdge> getEdges() {
         Collection<IEdge> edges = new HashSet<>();
         for (INode node : nodes) {

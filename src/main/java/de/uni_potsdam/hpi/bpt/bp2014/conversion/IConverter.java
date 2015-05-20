fec793f9a9ef496f8077ba117baa04a0b6e60b87
shaarmann <stephan.haarmann@student.hpi.uni-potsdam.de>
2015-05-20 17:18:40 +0200

    Added java doc to IConverter

--- src/main/java/de/uni_potsdam/hpi/bpt/bp2014/conversion/IConverter.java
+++ src/main/java/de/uni_potsdam/hpi/bpt/bp2014/conversion/IConverter.java
@@ -1,6 +1,19 @@
 package de.uni_potsdam.hpi.bpt.bp2014.conversion;
 
+/**
+ * An interface for the converter.
+ * A generic implementation to convert between different Process Model representations.
+ * If you transform exactly one process model into exactly one model of another
+ * representation you may use this interface.
+ */
 public interface IConverter {
 
+    /**
+     * This method should convert the model to a new model of type t/T.
+     * @param model The model to be transformed.
+     * @param t The type of the new model.
+     * @param <T> The returned type.
+     * @return
+     */
     <T extends IModel> T convert(IModel model, Class<T> t);
 }

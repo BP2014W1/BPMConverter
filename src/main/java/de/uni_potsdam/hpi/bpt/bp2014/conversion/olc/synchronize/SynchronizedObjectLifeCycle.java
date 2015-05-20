9b3157fe85d44cecd65f58eac200c756ea09e0e3
shaarmann <stephan.haarmann@student.hpi.uni-potsdam.de>
2015-05-20 09:23:13 +0200

    Added first comments to SyncrhonizedObjectLifeCycle

--- src/main/java/de/uni_potsdam/hpi/bpt/bp2014/conversion/olc/synchronize/SynchronizedObjectLifeCycle.java
+++ src/main/java/de/uni_potsdam/hpi/bpt/bp2014/conversion/olc/synchronize/SynchronizedObjectLifeCycle.java
@@ -9,14 +9,19 @@ import de.uni_potsdam.hpi.bpt.bp2014.conversion.olc.StateTransition;
 import java.util.*;
 
 /**
- * Created by Stpehan on 01.04.2015.
+ * This class represents a Synchronized Object Life Cycle.
+ * A Synchronized Object Life Cycle Aggregates a multiple Object Life Cycles
+ * and holds additional synchronization edges between state transitions.
  */
 public class SynchronizedObjectLifeCycle implements IModel {
+
+    private List<ObjectLifeCycle> objectLifeCycles;
+
+    private Map<StateTransition, List<StateTransition>> synchronisationEdges;
+
     public void setObjectLifeCycles(List<ObjectLifeCycle> objectLifeCycles) {
         this.objectLifeCycles = objectLifeCycles;
     }
-
-    private List<ObjectLifeCycle> objectLifeCycles;
 
     public Map<StateTransition, List<StateTransition>> getSynchronisationEdges() {
         return synchronisationEdges;
@@ -25,8 +30,6 @@ public class SynchronizedObjectLifeCycle implements IModel {
     public void setSynchronisationEdges(Map<StateTransition, List<StateTransition>> synchronisationEdges) {
         this.synchronisationEdges = synchronisationEdges;
     }
-
-    private Map<StateTransition, List<StateTransition>> synchronisationEdges;
 
     public SynchronizedObjectLifeCycle() {
         objectLifeCycles = new ArrayList<>();
@@ -47,6 +50,10 @@ public class SynchronizedObjectLifeCycle implements IModel {
         return nodes;
     }
 
+    /**
+     * it is not possible to add new Nodes.
+     * @param newNode This parameter will be ignored.
+     */
     @Override
     public void addNode(INode newNode) {
         // Do nothing
@@ -61,21 +68,39 @@ public class SynchronizedObjectLifeCycle implements IModel {
         return nodes;
     }
 
+    /**
+     * There is no specific start state.
+     * @return Will always be null.
+     */
     @Override
     public INode getStartNode() {
         return null;
     }
 
+    /**
+     * it is not possible to set a start state.
+     * @param startNode This parameter will be ignored.
+     */
     @Override
     public void setStartNode(INode startNode) {
         // Not supported
     }
 
+    /**
+     * it is not possible to add a final state.
+     * @param finalNode This parameter will be ignored.
+     */
     @Override
     public void addFinalNode(INode finalNode) {
         // Not supported
     }
 
+    /**
+     * Accumulates the final nodes of all Object Life cycles being
+     * part of this synchronized object life cycle.
+     * @return Returns the final nodes of all Object Life Cycles
+     * of this Synchronized Object Life Cycle.
+     */
     @Override
     public List<INode> getFinalNodes() {
         List<INode> nodes = new LinkedList<>();
@@ -85,6 +110,12 @@ public class SynchronizedObjectLifeCycle implements IModel {
         return nodes;
     }
 
+    /**
+     * Accumulates the final nodes of the specified Type of all Object Life cycles
+     * being part of this synchronized object life cycle.
+     * @return Returns the final nodes of all Object Life Cycles
+     * of this Synchronized Object Life Cycle if they hold the selection ondition.
+     */
     @Override
     public <T extends INode> List<T> getFinalNodesOfClass(Class t) {
         List<T> nodes = new LinkedList<>();
@@ -94,6 +125,15 @@ public class SynchronizedObjectLifeCycle implements IModel {
         return nodes;
     }
 
+    /**
+     * Returns all edges of the all Synchronized Object life Cycle, if they
+     * are instances of the specified type. Therefore they call the same
+     * method on each object life cycle.
+     * @param t Specifies the edge type.
+     * @param <T> Specifies the return type.
+     * @return Returns all edges of the all Synchronized Object life Cycle
+     * which fullfill the condition.
+     */
     public <T extends IEdge> List<T> getEdgesOfType(Class t) {
         Set<T> edges = new HashSet<>();
         for (INode node : getNodes()) {

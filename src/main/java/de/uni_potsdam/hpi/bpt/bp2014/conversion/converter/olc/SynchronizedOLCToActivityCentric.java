3602031f13dee7204fa2ffdf083da85049d34e66
shaarmann <stephan.haarmann@student.hpi.uni-potsdam.de>
2015-05-20 08:46:31 +0200

    converter.olc package has been documentdAdded Java doc to Synchronized

--- src/main/java/de/uni_potsdam/hpi/bpt/bp2014/conversion/converter/olc/SynchronizedOLCToActivityCentric.java
+++ src/main/java/de/uni_potsdam/hpi/bpt/bp2014/conversion/converter/olc/SynchronizedOLCToActivityCentric.java
@@ -13,8 +13,24 @@ import de.uni_potsdam.hpi.bpt.bp2014.conversion.olc.synchronize.SynchronizedObje
 
 import java.util.*;
 
+/**
+ * This model is a converter for synchronized Object Life Cycles.
+ * It generates one {@link ActivityCentricProcessModel} based on one
+ * {@link SynchronizedObjectLifeCycle}.
+ * State transitions will be mapped on Activities and Data Objects nodes
+ * on Data Object States.
+ */
 public class SynchronizedOLCToActivityCentric implements IConverter {
+    /**
+     * The Synchronized Object Life Cycle which will be converted.
+     */
     private SynchronizedObjectLifeCycle synchronizedObjectLifeCycle;
+    /**
+     * A list of ActivityBuilders representing activities in the
+     * generated model. Each ActivityBuilder will be checked once
+     * for successors. This List holds all the nodes which have
+     * to be checked.
+     */
     private Collection<ActivityBuilder> nodesToBeChecked;
 
     /**
@@ -41,11 +57,31 @@ public class SynchronizedOLCToActivityCentric implements IConverter {
     }
 
 
+    /**
+     * This method sets {@link #synchronizedObjectLifeCycle} and then calls the
+     * {@link #convert()} method in order to generate an {@link ActivityCentricProcessModel}
+     * The result will be returned.
+     *
+     * @param sOLC The Synchronized Object Life Cycle which is the base for the conversion.
+     * @return Returns the generated Activity Centric Process Model.
+     */
     public ActivityCentricProcessModel convert(SynchronizedObjectLifeCycle sOLC) {
         synchronizedObjectLifeCycle = sOLC;
         return convert();
     }
 
+    /**
+     * This methods creates a new activity centric process model.
+     * It uses a synchronized object life cycle as an input, which
+     * must be defined in {@link #synchronizedObjectLifeCycle}.
+     * Other attributes of this class will be initialized and changed
+     * during that process.
+     * You may not manipulate this process.
+     * The conversion is an implementation of the algorithm described
+     * in {@see http://bpt.hpi.uni-potsdam.de/pub/Public/AndreasMeyer/Technical_Report_Activity-centric_and_Artifact-centric_Process_Model_Roundtrip.pdf}.
+     *
+     * @return The generated ActivityCentricProcessModel.
+     */
     public ActivityCentricProcessModel olcToACP(SynchronizedObjectLifeCycle sOLC)
             throws InstantiationException, IllegalAccessException {
         OLCConversionFlyweight<ActivityCentricProcessModel> flyweight =
@@ -71,7 +107,7 @@ public class SynchronizedOLCToActivityCentric implements IConverter {
             return flyweight.getModelUnderConstruction();
         } else {
             Gateway gateway = new Gateway();
-            if (nodesToBeCheckedAreDisjoint()) {
+            if (nodesToBeCheckedAreConcurrent()) {
                 gateway.setType(Gateway.Type.AND);
             } else {
                 gateway.setType(Gateway.Type.XOR);
@@ -128,7 +164,14 @@ public class SynchronizedOLCToActivityCentric implements IConverter {
         return flyweight.getModelUnderConstruction();
     }
 
-    private boolean nodesToBeCheckedAreDisjoint() {
+    /**
+     * Checks weather or not to nodes are concurrent.
+     * Two nodes are concurrent if and only if there input Sets
+     * are disjoint.
+     *
+     * @return Returns false if they have shared data inputs, else true.
+     */
+    private boolean nodesToBeCheckedAreConcurrent() {
         for (ActivityBuilder node1 : nodesToBeChecked) {
             for (ActivityBuilder node2 : nodesToBeChecked) {
                 if (!node1.equals(node2) && !node1.inputSetsAreDisjoint(node2)) {
@@ -139,6 +182,14 @@ public class SynchronizedOLCToActivityCentric implements IConverter {
         return true;
     }
 
+    /**
+     * Initializes the nodes to be checked by adding all initial DataObject
+     * states to the enabled states and than determining the enabled Activities.
+     * The flyweight is needed in order to receive the ActivityBuilder for a
+     * combined Transition.
+     *
+     * @param flyweight The Flyweight which holds shared resources.
+     */
     private void initNodesToBeChecked(
             OLCConversionFlyweight<ActivityCentricProcessModel> flyweight) {
         Collection<DataObjectState> startStates = new HashSet<>();
@@ -157,7 +208,7 @@ public class SynchronizedOLCToActivityCentric implements IConverter {
     @Override
     public <T extends IModel> T convert(IModel model, Class<T> t) {
         assert model instanceof SynchronizedObjectLifeCycle :
-                "The given model is no synchrnoized OLC";
+                "The given model is not a synchronized OLC";
         synchronizedObjectLifeCycle = (SynchronizedObjectLifeCycle) model;
         return (T) convert();
     }

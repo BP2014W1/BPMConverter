8f3bb45bd373817c4b3932787de872cbf035112e
shaarmann <stephan.haarmann@student.hpi.uni-potsdam.de>
2015-05-23 12:10:32 +0200

    Small bug fix Loop transitions will not be added

--- src/main/java/de/uni_potsdam/hpi/bpt/bp2014/conversion/converter/activity_centric/ActivityCentricToSynchronizedOLC.java
+++ src/main/java/de/uni_potsdam/hpi/bpt/bp2014/conversion/converter/activity_centric/ActivityCentricToSynchronizedOLC.java
@@ -162,12 +162,14 @@ public class ActivityCentricToSynchronizedOLC implements IConverter {
             for (DataObjectState predecessor : entry.getValue()) {
                 if (currentStates.containsKey(entry.getKey())) {
                     for (DataObjectState successor : currentStates.get(entry.getKey())) {
-                        StateTransition transition =
-                                new StateTransition(predecessor,
-                                        successor,
-                                        (node == null) ? "t" : ((Activity) node).getName());
-                        predecessor.addOutgoingEdge(transition);
-                        successor.addIncomingEdge(transition);
+                        if (node != null || !predecessor.equals(successor)) {
+                            StateTransition transition =
+                                    new StateTransition(predecessor,
+                                            successor,
+                                            (node == null) ? "t" : ((Activity) node).getName());
+                            predecessor.addOutgoingEdge(transition);
+                            successor.addIncomingEdge(transition);
+                        }
                     }
                 }
             }

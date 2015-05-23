e32b28961c90cc4646e6457aa5c9d9af0003ecbb
shaarmann <stephan.haarmann@student.hpi.uni-potsdam.de>
2015-05-23 13:05:08 +0200

    Refactored SynchronizedOLCToActivityCentric.java

--- src/main/java/de/uni_potsdam/hpi/bpt/bp2014/conversion/converter/olc/SynchronizedOLCToActivityCentric.java
+++ src/main/java/de/uni_potsdam/hpi/bpt/bp2014/conversion/converter/olc/SynchronizedOLCToActivityCentric.java
@@ -10,7 +10,9 @@ import de.uni_potsdam.hpi.bpt.bp2014.conversion.olc.DataObjectState;
 import de.uni_potsdam.hpi.bpt.bp2014.conversion.olc.ObjectLifeCycle;
 import de.uni_potsdam.hpi.bpt.bp2014.conversion.olc.synchronize.SynchronizedObjectLifeCycle;
 
-import java.util.*;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.LinkedList;
 
 /**
  * This model is a converter for synchronized Object Life Cycles.
@@ -51,7 +53,8 @@ public class SynchronizedOLCToActivityCentric implements IConverter {
             e.printStackTrace();
         } catch (IllegalAccessException e) {
             e.printStackTrace();
-        };
+        }
+        ;
         return null;
     }
 
@@ -83,35 +86,128 @@ public class SynchronizedOLCToActivityCentric implements IConverter {
      */
     public ActivityCentricProcessModel olcToACP(SynchronizedObjectLifeCycle sOLC)
             throws InstantiationException, IllegalAccessException {
-        OLCConversionFlyweight<ActivityCentricProcessModel> flyweight =
-                new OLCConversionFlyweight<>(sOLC,
-                        ActivityCentricProcessModel.class);
+        OLCConversionFlyweight<ActivityCentricProcessModel> flyweight = initializeFlyweight(sOLC);
+        new OLCConversionFlyweight<>(sOLC,
+                ActivityCentricProcessModel.class);
         Event startEvent = new Event();
         startEvent.type = Event.Type.START;
         initNodesToBeChecked(flyweight);
         Collection<ActivityBuilder> nodeBuilderNodesChecked = new HashSet<>();
+        if (nodesToBeChecked.isEmpty()) {
+            return flyweight.getModelUnderConstruction();
+        }
+        boolean exclusive = false;
+        if (!flyweight.getModelUnderConstruction().getNodesOfClass(Gateway.class).isEmpty() &&
+                ((Gateway) flyweight.getModelUnderConstruction().getNodesOfClass(Gateway.class)
+                        .iterator().next()).getType().equals(Gateway.Type.XOR)) {
+            exclusive = true;
+        }
+        do {
+            Collection<CombinedTransition> concurrentCombinedTransitions =
+                    determineConcurrentCTs(exclusive);
+            nodeBuilderNodesChecked.addAll(determineEnabledCTsAndCheckedNodes(concurrentCombinedTransitions));
+            Collection<ActivityBuilder> newNodes = getNewNodes();
+            nodesToBeChecked.removeAll(nodeBuilderNodesChecked);
+            nodesToBeChecked.addAll(newNodes);
+            establishControlFlow(nodeBuilderNodesChecked);
+        } while (!nodesToBeChecked.isEmpty());
+        flyweight.finalizeModel();
+        return flyweight.getModelUnderConstruction();
+    }
+
+    /**
+     * This method establishes the control Flow between the checked nodes
+     * and their predecessors.
+     *
+     * @param nodeBuilderNodesChecked Represents the nodes which have been checked before this iteration.
+     */
+    private void establishControlFlow(Collection<ActivityBuilder> nodeBuilderNodesChecked) {
+        for (ActivityBuilder nodeBuilder : nodesToBeChecked) {
+            nodeBuilder.establishIncomingControlFlow();
+        }
+        for (ActivityBuilder nodeBuilder : nodeBuilderNodesChecked) {
+            nodeBuilder.establishIncomingControlFlow();
+        }
+    }
+
+    /**
+     * This method does two things on the one hand it checks weather or not an
+     * activity has been checked for combined transition.
+     * If it has not been checked, it will be checked.
+     *
+     * @param concurrentCombinedTransitions The concurrentCombinedTransitions,
+     *                                      mandatory to reduce the possible enabled transitions.
+     * @return Returns a collection of ActivityBuilders which have already been
+     * checked for combined transitions before.
+     */
+    private Collection<? extends ActivityBuilder> determineEnabledCTsAndCheckedNodes(
+            Collection<CombinedTransition> concurrentCombinedTransitions) {
+        Collection<ActivityBuilder> nodesChecked = new HashSet<>();
+        for (ActivityBuilder activityBuilder : nodesToBeChecked) {
+            if (activityBuilder.isChecked()) {
+                nodesChecked.add(activityBuilder);
+            } else {
+                activityBuilder
+                        .findEnabledCombinedTransitions()
+                        .findPossibleEnabledCombinedTransitions(
+                                concurrentCombinedTransitions);
+            }
+        }
+        return nodesChecked;
+    }
+
+    /**
+     * This method determines all concurrent combined transitions.
+     *
+     * @param exclusive Indicates weather or not the latest Activities are concurrent.
+     * @return A list of concurrent combined transition. If there are non an empty list
+     * will be returned.
+     */
+    private Collection<CombinedTransition> determineConcurrentCTs(boolean exclusive) {
+        Collection<CombinedTransition> concurrentCombinedTransitions = new HashSet<>();
+        if (!exclusive) {
+            for (ActivityBuilder activityBuilder : nodesToBeChecked) {
+                if (!activityBuilder.isChecked()) {
+                    concurrentCombinedTransitions.add(activityBuilder.getCtExecuted());
+                }
+            }
+        }
+        return concurrentCombinedTransitions;
+    }
+
+    /**
+     * This method initializes the Flyweight.
+     * First of all it creates a new Flyweight object for the given Synchronized Object Life Cycle.
+     * Then the nodesToBeChecked will be initialized.
+     * If there are no nodes a minimalistic flyweight will be created.
+     * Else the first Activity/Activities will be created and added.
+     * If there are more than one an Gateway (split/fork) will be established before
+     * the Gateway.
+     *
+     * @param sOLC The Synchronized Object Life Cycle to be created.
+     * @return The Flyweight which has been created and initialized.
+     * @throws InstantiationException The Flyweight could not be created, due to an instantiation Exception.
+     * @throws IllegalAccessException ThE Flyweight could not be created, because accessing the constructor of
+     *                                the model type was forbidden.
+     */
+    private OLCConversionFlyweight<ActivityCentricProcessModel> initializeFlyweight(SynchronizedObjectLifeCycle sOLC)
+            throws InstantiationException, IllegalAccessException {
+        OLCConversionFlyweight<ActivityCentricProcessModel> flyweight = new OLCConversionFlyweight<>(sOLC,
+                ActivityCentricProcessModel.class);
+        Event startEvent = new Event();
+        startEvent.type = Event.Type.START;
+        initNodesToBeChecked(flyweight);
         flyweight.getModelUnderConstruction().addNode(startEvent);
         flyweight.getModelUnderConstruction().setStartNode(startEvent);
-        boolean exclusive = false;
         if (nodesToBeChecked.size() == 1) {
             nodesToBeChecked.iterator().next().addPredecessor(startEvent);
         } else if (nodesToBeChecked.size() < 1) {
-            Event endEvent = new Event();
-            endEvent.setType(Event.Type.END);
-            ControlFlow cf = new ControlFlow(startEvent, endEvent);
-            startEvent.addOutgoingEdge(cf);
-            endEvent.addIncomingEdge(cf);
-            flyweight.getModelUnderConstruction().addFinalNode(endEvent);
-            flyweight.getModelUnderConstruction().addNode(endEvent);
-            return flyweight.getModelUnderConstruction();
+            createNoActivityModel(startEvent, flyweight);
         } else {
             Gateway gateway = new Gateway();
-            if (nodesToBeCheckedAreConcurrent()) {
-                gateway.setType(Gateway.Type.AND);
-            } else {
-                gateway.setType(Gateway.Type.XOR);
-                exclusive = true;
-            }
+            gateway.setType(nodesToBeCheckedAreConcurrent() ?
+                    Gateway.Type.AND :
+                    Gateway.Type.XOR);
             ControlFlow incoming = new ControlFlow(startEvent, gateway);
             gateway.addIncomingEdge(incoming);
             startEvent.addOutgoingEdge(incoming);
@@ -120,47 +216,25 @@ public class SynchronizedOLCToActivityCentric implements IConverter {
             }
             flyweight.getModelUnderConstruction().addNode(gateway);
         }
-        do {
-            Collection<CombinedTransition> concurrentCombinedTransitions =
-                    new HashSet<>();
-            if (!exclusive) {
-                for (ActivityBuilder activityBuilder : nodesToBeChecked) {
-                    if (!activityBuilder.isChecked()) {
-                        concurrentCombinedTransitions.add(activityBuilder.getCtExecuted());
-                    }
-                }
-            }
-            for (ActivityBuilder activityBuilder : nodesToBeChecked) {
-                if (activityBuilder.isChecked()) {
-                    nodeBuilderNodesChecked.add(activityBuilder);
-                } else {
-                    activityBuilder
-                            .findEnabledCombinedTransitions()
-                            .findPossibleEnabledCombinedTransitions(
-                                    concurrentCombinedTransitions);
-                }
-            }
-            nodesToBeChecked.removeAll(nodeBuilderNodesChecked);
-            Collection<ActivityBuilder> newNodes = new HashSet<>();
-            for (ActivityBuilder activityBuilder : nodesToBeChecked) {
-                Collection<ActivityBuilder> successors = activityBuilder.getSuccessorActivities();
-                activityBuilder.establishOutgoingControlFlow();
-                for (ActivityBuilder successor : successors) {
-                    if (!successor.isChecked()) {
-                        newNodes.add(successor);
-                    }
-                }
-            }
-            nodesToBeChecked.addAll(newNodes);
-            for (ActivityBuilder nodeBuilder : nodesToBeChecked) {
-                nodeBuilder.establishIncomingControlFlow();
-            }
-            for (ActivityBuilder nodeBuilder : nodeBuilderNodesChecked) {
-                nodeBuilder.establishIncomingControlFlow();
-            }
-        } while (!nodesToBeChecked.isEmpty());
-        flyweight.finalizeModel();
-        return flyweight.getModelUnderConstruction();
+        return flyweight;
+    }
+
+    /**
+     * Adds start and end event to the flyweight connected by one Control Flow edge.
+     * Results in a minimalistic Process model. No other nodes or eges will be added.
+     * The changes will be saved to the flyweight.
+     *
+     * @param startEvent The start event created for the flyweight.
+     * @param flyweight  The flyweight to be altered.
+     */
+    private void createNoActivityModel(Event startEvent, OLCConversionFlyweight<ActivityCentricProcessModel> flyweight) {
+        Event endEvent = new Event();
+        endEvent.setType(Event.Type.END);
+        ControlFlow cf = new ControlFlow(startEvent, endEvent);
+        startEvent.addOutgoingEdge(cf);
+        endEvent.addIncomingEdge(cf);
+        flyweight.getModelUnderConstruction().addFinalNode(endEvent);
+        flyweight.getModelUnderConstruction().addNode(endEvent);
     }
 
     /**
@@ -210,5 +284,26 @@ public class SynchronizedOLCToActivityCentric implements IConverter {
                 "The given model is not a synchronized OLC";
         synchronizedObjectLifeCycle = (SynchronizedObjectLifeCycle) model;
         return (T) convert();
+    }
+
+    /**
+     * Based on the nodes to be checked the new nodes will be determined.
+     * A node is new if and only if it is a successor of at least one node of {@link #nodesToBeChecked}
+     * and if the node has not been checked before.
+     *
+     * @return A Collection of all nodes which have been created during this iteration.
+     */
+    public Collection<ActivityBuilder> getNewNodes() {
+        Collection<ActivityBuilder> newNodes = new HashSet<>();
+        for (ActivityBuilder activityBuilder : nodesToBeChecked) {
+            Collection<ActivityBuilder> successors = activityBuilder.getSuccessorActivities();
+            activityBuilder.establishOutgoingControlFlow();
+            for (ActivityBuilder successor : successors) {
+                if (!successor.isChecked()) {
+                    newNodes.add(successor);
+                }
+            }
+        }
+        return newNodes;
     }
 }
